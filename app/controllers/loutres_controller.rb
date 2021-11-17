169852e6cb263fa6d6b215caf3a3c64901239733
Arthur Ancian <arthurancian@gmail.com>
2021-11-17 12:18:27 +0100

    conflict

--- app/controllers/loutres_controller.rb
+++ app/controllers/loutres_controller.rb
@@ -6,10 +6,7 @@ class LoutresController < ApplicationController
 
   def show
     @loutre = Loutre.find(params[:id])
-<<<<<<< HEAD
     # @loutre.user = current_user
-=======
     authorize @loutre
->>>>>>> 7555826ba5ead1607faa9b8cea03c3401539ea82
   end
 end
