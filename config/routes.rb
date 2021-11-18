9debb1e7d66d59cb85c71140fea2879bc5356104
Mathieu Romano <mathieuromano3@gmail.com>
2021-11-18 15:20:51 +0100

    dasboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  #root to: 'pages#dashboard'
+  get 'dashboard', to: 'pages#dashboard'
   resources :users
   resources :loutres do
     resources :locations, only: %i[new create]
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,10 @@ puts 'create 5 users'
 end
 
 delphine = User.create!(email: "delphine@gmail.com", password: "delphine", first_name: "delphine", last_name: "cph", description: "blabla")
+mathieu = User.create!(email: "mathieu@gmail.com", password: "mathieu", first_name: "mathieu", last_name: "Rmn", description: "blabla")
 
-puts 'create 10 loutres'
+
+puts 'create 8 loutres'
 
 CATEGORY = ["party", "travel", "restaurant"]
 PERSONALITY = ["angry", "happy"]
@@ -38,7 +40,7 @@ images = ['https://geo.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-w
     'https://cdn.pixabay.com/photo/2019/08/22/14/08/asian-short-clawed-otter-4423602__480.jpg',
     'https://cdn.pixabay.com/photo/2014/06/09/12/30/otter-365371_1280.jpg']
 
-10.times do
+5.times do
   loutre = Loutre.new(
     name: Faker::Creature::Cat.name,
     image: images.sample,
@@ -50,17 +52,30 @@ images = ['https://geo.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-w
   loutre.save!
 end
 
+3.times do
+  loutre = Loutre.new(
+    name: Faker::Creature::Cat.name,
+    image: images.sample,
+    price: rand(50..100),
+    event: CATEGORY.sample,
+    personality: PERSONALITY.sample,
+    user: mathieu
+  )
+  loutre.save!
+end
+
+
 puts 'create 5 locations'
 
-5.times do
-  location = Location.new(
-    date_départ: Date.new(2021, 11, 17),
-    date_arrive: Date.new(2021, 11, 18),
-    status: false,
-    user: delphine,
-    loutre: Loutre.all.sample
-  )
-  location.save!
+3.times do
+location = Location.new(
+  date_départ: Date.new(2021, 11, 17),
+  date_arrive: Date.new(2021, 11, 18),
+  status: false,
+  user: delphine,
+  loutre: Loutre.all.sample
+)
+location.save!
 end
 
 puts 'done'
