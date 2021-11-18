9680ac3c23a4a79184f3eca3a0e02dd8095213d4
Lucien Dupre <duprelucien@gmail.com>
2021-11-18 15:44:58 +0100

    résolution de conflit

--- app/models/loutre.rb
+++ app/models/loutre.rb
@@ -1,9 +1,6 @@
 class Loutre < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
   has_one_attached :photo
-=======
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
->>>>>>> master
 end
