55929b7f1974f370fa6aac6c75a2bf51de0cbb55
Lucien Dupre <duprelucien@gmail.com>
2021-11-18 12:27:37 +0100

    create otter

--- app/controllers/loutres_controller.rb
+++ app/controllers/loutres_controller.rb
@@ -8,4 +8,26 @@ class LoutresController < ApplicationController
     @loutre = Loutre.find(params[:id])
     authorize @loutre
   end
+
+  def new
+    @loutre = Loutre.new
+    @user = current_user
+    authorize @loutre
+  end
+
+  def create
+    @loutre = current_user.loutres.new(loutre_params)
+    authorize @loutre
+    if @loutre.save!
+      flash[:notice] = 'Votre annonce a bien été enrengistrée.'
+    else
+      render :new
+    end
+  end
 end
+
+  private
+
+  def loutre_params
+    params.require(:loutre).permit(:name, :personality, :event, :price, :image)
+  end
