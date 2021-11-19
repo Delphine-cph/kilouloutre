19df53b433452fa635a3948d3fcc0893d0e00278
Mathieu Romano <mathieuromano3@gmail.com>
2021-11-19 15:49:19 +0100

    card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,12 @@ class PagesController < ApplicationController
 
   def dashboard
     @loutres = policy_scope(Loutre).where(user: current_user)
-    @locations = Location.where(user: current_user)
+    @locations = []
+    @loutres.each do |loutre|
+      loutre.locations.each do |location|
+        @locations << location
+      end
+    end
     @loutre = Loutre.new
   end
 end
